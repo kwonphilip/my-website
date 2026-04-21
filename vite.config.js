6cbe1c50dff0347f41c19e5577ff948f5a50f613
beepboop <35923674+kwonphilip@users.noreply.github.com>
2026-04-21 14:11:59 -0400

    chore: update config for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-website/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
